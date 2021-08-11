eac5d136e6b0f9c290f3554095e1e1a4dd15c194
alex-1134 <alexa.t.lach@gmail.com>
2021-08-11 09:38:41 +0000

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,2 @@
+const cardSelection = ['one', 'one', 'two', 'two', 'three', 'three', 'four', 'four', 'five', 'five', 'six', 'six',
+ 'seven', 'seven', 'eight', 'eight', 'nine', 'nine', 'ten', 'ten', 'eleven', 'eleven', 'twelve', 'twelve']
