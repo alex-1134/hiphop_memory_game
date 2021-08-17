d5352a8d7579c8c39c16f5d8bcb3c8469bff2d4b
alex-1134 <alexa.t.lach@gmail.com>
2021-08-17 16:59:58 +0000

    Change order of some functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,35 @@
 const picSelection = ['one', 'one', 'two', 'two', 'three', 'three', 'four', 'four', 'five', 'five', 'six', 'six',
  'seven', 'seven', 'eight', 'eight', 'nine', 'nine', 'ten', 'ten', 'eleven', 'eleven', 'twelve', 'twelve'];
 
-let pics = [...document.querySelectorAll('div')];
+let pics = [document.querySelectorAll('div')].className('hidden');
 
+const beginGame = new Date().getTime();
+
+let chosenPic = '';
+const chosenPics = [];
+let gameResult = 0;
+const twoPics = pics.length/2; /* 12 pairs of pictures */
+
+/* Choosing random picture */
+const start = function() {
+  pics.forEach(function(pic) {
+      const position = Math.floor(Math.random() * picSelection.length);
+      pic.classList.add(picSelection[position]);
+      picSelection.splice(position, 1);
+  });
+
+  /* Shows for how long both cards will be visible (if not a match) */
+  setTimeout(function() {
+    pics.forEach(function(pic) {
+      pic.classList.add('hidden');
+      pic.addEventListener('click', pickPic);
+    });
+  }, 1500) ;
+};
 
 /* This function allows the player to choose a card - works with the EventListener below */
 const pickPic = function() {
-  chosenPic = this;
+  chosenPic = this.addClassName('picSelection');
 
 /* Won't allow us to click twice on the same picture */  
   if(chosenPic == chosenPics[0]) 
@@ -22,64 +45,38 @@ const pickPic = function() {
 
   /* Second click */  
   } else {
-      pics.forEach(pic => pic.removeEventListener('click', pickPic))
+      pics.forEach(pic => pic.removeEventListener('click', pickPic));
       chosenPics[1] = chosenPic;
+
       setTimeout(function () {
         if (chosenPics[0].className === chosenPics[1].className) {
-          console.log('WINNING!!!');
-          chosenPics.forEach(pic => pic.classList.add('matched'));
+          alert('WINNING!!!');
+          chosenPics.forEach(pic => pic.classList.add('matched')); /* Turn the guessed pictures into white cards */
           gameResult++;
 
           pics = pics.filter(pic => !pic.classList.contains('matched')); /* Exclude these elements from the game */
 
           if (gameResult == twoPics) {
             const endGame = new Date().getTime();
-            const gameTime = (endGame - beginGame) / 1000;
+            const gameTime = (endGame - beginGame) / 1000; /* Shows the game time it in seconds */
             alert(`Your time: ${gameTime}s!`);
-            location.reload();
+            location.reload(); /* Start new game */
           }
+
         } else {
-          console.log('NOT THIS TIME, BUDDY!');
-          chosenPics.forEach(pic => pic.classList.add('hidden'))
+          alert('NOT THIS TIME, BUDDY!');
+          chosenPics.forEach(pic => pic.classList.add('hidden')) /* Turns the pictures back to pink */
         }
 
         chosenPic = '';
         chosenPic.length = 0;
         pics.forEach(pic => pic.addEventListener('click', pickPic));
 
-
       }, 3000);
     }
   };        
 
 
-let chosenPic = '';
-const chosenPics = [];
 
-let gameResult = 0;
-
-const twoPics = pics.length/2; /* 12 pairs of pictures */
-
-const beginGame = new Date().getTime();
-
-
-/* Choosing random picture */
-const start = function() {
-  pics.forEach(function(pic) {
-      const position = Math.floor(Math.random() * picSelection.length);
-      pic.classList.add(picSelection[position]);
-      picSelection.splice(position, 1);
-  });
-
-  /* Shows for how long both cards will be visible (if not a match) */
-  setTimeout(function() {
-    pics.forEach(function(pic) {
-      pic.classList.add('hidden');
-      pic.addEventListener('click', pickPic)
-    });
-  }, 1500) 
-}
-
-
-start()
+start();
 
