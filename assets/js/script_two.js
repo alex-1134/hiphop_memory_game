0d66c780f42fda48ace21e357630370cc40fbc52
alex-1134 <alexa.t.lach@gmail.com>
2021-08-21 10:49:11 +0000

    Style play buttons

--- assets/js/script_two.js
+++ assets/js/script_two.js
@@ -4,83 +4,84 @@ const picSelection = ['pic-1', 'pic-1', 'pic-2', 'pic-2', 'pic-3', 'pic-3', 'pic
  'pic-10', 'pic-10', 'pic-11', 'pic-11', 'pic-12', 'pic-12','pic-13', 'pic-13', 'pic-14', 'pic-14',
  'pic-15', 'pic-15', 'pic-16', 'pic-16', 'pic-17', 'pic-17', 'pic-18', 'pic-18'];
 
-// Select all divs 
-let pics = [...document.querySelectorAll('div')]; 
+ // Select all divs 
+ let pics = [...document.querySelectorAll('div')]; 
 
-// Get the time when the game begins
-const beginGame = new Date().getTime();
-
-
-// This function is based on functions from https://www.youtube.com/watch?v=gKUUHjEg7mQ&t=3902s  
-const start = function () {
-   pics.forEach(pic => {
-       const position = Math.floor(Math.random() * picSelection.length); //1
-       pic.classList.add(picSelection[position]);
-       picSelection.splice(position, 1);
-   })
-   setTimeout(function () {
-       pics.forEach(pic => {
-           pic.classList.add("hidden")
-           pic.addEventListener("click", clickPic)
-       })
-   }, 1500)
- };
+ // Get the time when the game begins
+ const beginGame = new Date().getTime();
  
- start()
-
- // Place two chosen pictures here
- const chosenPics = [];
-
- // Shows which picture has been clicked on
- let chosenPic = '';
  
- const clickPic = function () {
+ // This function is based on functions from https://www.youtube.com/watch?v=gKUUHjEg7mQ&t=3902s  
+ const start = function () {
+    pics.forEach(pic => {
+        const position = Math.floor(Math.random() * picSelection.length); //1
+        pic.classList.add(picSelection[position]);
+        picSelection.splice(position, 1);
+    })
+    setTimeout(function () {
+        pics.forEach(pic => {
+            pic.classList.add("hidden")
+            pic.addEventListener("click", clickPic)
+        })
+    }, 1000)
+  };
+  
+  start()
+ 
+  // Place two chosen pictures here
+  const chosenPics = [];
 
+  // Shows which picture has been clicked on
+  let chosenPic = '';
+  
+  const clickPic = function () {
+ 
 chosenPic = this;
 // Which picture was chosen
 if (chosenPic == chosenPics[0]) return; // In case the same element gets chosen twice
-
+ 
 chosenPic.classList.remove('hidden');
 // First click, array = 0
 // These two functions are based on functions from https://www.youtube.com/watch?v=gKUUHjEg7mQ&t=3902s
-  if (chosenPics.length === 0) {
-      console.log("First");
-      chosenPics[0] = chosenPic; 
-      return;
-  } else {
-      console.log("Second");
-       // For a brief moment we remove the possibility to click 
-      pics.forEach(pic => pic.removeEventListener("click", clickPic))
-      chosenPics[1] = chosenPic;
+   if (chosenPics.length === 0) {
+       console.log("First");
+       chosenPics[0] = chosenPic; 
+       return;
+   } else {
+       console.log("Second");
+        // For a brief moment we remove the possibility to click 
+       pics.forEach(pic => pic.removeEventListener("click", clickPic))
+       chosenPics[1] = chosenPic;
+ 
+       setTimeout(function () {
+           if (chosenPics[0].className === chosenPics[1].className) {
+               console.log("WINNING!")
+               chosenPics.forEach(pic => pic.classList.add("matched"))
+               pics = pics.filter(pic => !pic.classList.contains("matched"));
+               gameResult++; // Update game result
 
-      setTimeout(function () {
-          if (chosenPics[0].className === chosenPics[1].className) {
-              console.log("WINNING!")
-              chosenPics.forEach(pic => pic.classList.add("matched"))
-              pics = pics.filter(pic => !pic.classList.contains("matched"));
-              gameResult++; // Update game result
+               // Check if the game came to an end
+               if (gameResult == gameLenght) {
+                   const endGame = new Date().getTime();
+                   const gameTime = (endGame - beginGame) / 1000
+                   alert(`You won! Your score is: ${gameTime} `)
+                   location.reload(); // Start the game again
+               }
+           } else {
+               console.log("Lost")
+               chosenPics.forEach(pic => pic.classList.add("hidden"))
+           }
 
-              // Check if the game came to an end
-              if (gameResult == gameLenght) {
-                  const endGame = new Date().getTime();
-                  const gameTime = (endGame - beginGame) / 1000
-                  alert(`You won! Your score is: ${gameTime} `)
-                  location.reload(); // Start the game again
-              }
-          } else {
-              console.log("Lost")
-              chosenPics.forEach(pic => pic.classList.add("hidden"))
-          }
+           chosenPic = ""; 
+           chosenPics.length = 0; 
+           pics.forEach(pic => pic.addEventListener("click", clickPic))
+ 
+       }, 700)
+     } 
+ };
+ 
+ const gameLenght = pics.length / 2; // 18 pairs
+ let gameResult = 0;
 
-          chosenPic = ""; 
-          chosenPics.length = 0; 
-          pics.forEach(pic => pic.addEventListener("click", clickPic))
-
-      }, 700)
-    } 
-};
-
-const gameLenght = pics.length / 2; // 18 pairs
-let gameResult = 0;
 
 
